fix: fix normalization in hstu_spring regularization

src/genrec/models/model_seqrec/sasrec2hstu_spring.py
@@ -346,8 +346,10 @@ class SASRec2HSTUSpringModel(SeqRecModel[SASRec2HSTUSpringModelConfig, SASRec2HS
         attn_weight = attn_weight.masked_fill(causal_mask.squeeze(1), 0.0)
 
         # normalize attention weights along the last dimension
-        key_sums: Float[torch.Tensor, "B L 1"] = attn_weight.sum(dim=-1, keepdim=True)
-        attn_weight = attn_weight / (key_sums + 1e-12)
+        # NOTE: we apply max-min normalization here, as silu may be negative, and the key_sums may be zero
+        key_max: Float[torch.Tensor, "B L 1"] = attn_weight.max(dim=-1, keepdim=True).values
+        key_min: Float[torch.Tensor, "B L 1"] = attn_weight.min(dim=-1, keepdim=True).values
+        attn_weight = (attn_weight - key_min) / (key_max - key_min + 1e-12)
 
         query_sums: Float[torch.Tensor, "B*L"] = attn_weight.sum(dim=-2).flatten()
         attention_mask_flat: Bool[torch.Tensor, "B*L"] = attention_mask.bool().flatten()